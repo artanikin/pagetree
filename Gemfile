20d09948a62cd518875631a9468427ca4736661d
Anikin Artyom <anikinartyom@gmail.com>
2013-07-08 07:32:56 +0600

    Initial commit

---
+++ Gemfile
@@ -0,0 +1,24 @@
+source 'http://rubygems.org'
+
+gem 'rails', '3.1.0'
+gem 'jquery-rails'
+
+group :development, :test do
+	gem 'sqlite3'
+	gem 'rspec-rails'
+end
+
+group :assets do
+  gem 'sass-rails', "  ~> 3.1.0"
+  gem 'coffee-rails', "~> 3.1.0"
+  gem 'uglifier'
+end
+
+group :test do
+  gem 'capybara', '1.1.2'
+end
+
+# group :production do
+# 	gem 'pg'
+# end
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pagetree::Application.config.secret_token = '91759732ec95716bba04774842a83107789fbfa7fb0aff4a552b20ea3b263da165c93bf653f8e4040c72d810f2b208cd3a9aad9230d428e159ca426d56798d09'
